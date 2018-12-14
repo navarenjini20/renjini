a2e963588afe7bf96ece8da7d0a3832bd4a0d4b4
navarenjini20 <45868307+navarenjini20@users.noreply.github.com>
2018-12-14 12:25:13 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,36 @@
+function validate form(){
+x=document get element BYZD("email")value
+if(x==""){
+alert("email must be filled");
+return false;
+}
+x=document get element BYZD("passward")value
+if(x==""){
+alert("passward must be filled");
+return false;
+}
+x=document get element BYZD("username")value
+if(x==""){
+alert("username must be filled");
+return false;
+}
+x=document get element BYZD("college name")value
+if(x==""){
+alert("college name must be filled");
+return false;
+}
+x=document get element BYZD("branch")value
+if(x==""){
+alert("branch must be filled");
+return false;
+}
+x=document get element BYZD("degree")value
+if(x==""){
+alert("degree must be filled");
+return false;
+}
+x=document get element BYZD("repeat passward")value
+if(x==""){
+alert("repeat passward must be filled");
+return false;
+}
