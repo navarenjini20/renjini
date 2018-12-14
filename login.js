4b4ac2f88d89598c787bbb940144fce22bdc6d33
navarenjini20 <45868307+navarenjini20@users.noreply.github.com>
2018-12-14 12:26:16 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+function validate form(){
+x=document get element BYZD("email")value
+if(x==""){
+alert("email must be filled");
+return false;
+}
+function validate form(){
+x=document get element BYZD("passward"value
+if(x==""){
+alert("passward must be filled");
+return false;
+}
