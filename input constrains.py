a182b99a477f842ea14d58ff92c7385559f5a49e
navarenjini20 <45868307+navarenjini20@users.noreply.github.com>
2019-03-07 15:04:17 +0530

    Create input constrains.py

---
+++ input constrains.py
@@ -0,0 +1,10 @@
+n = int(raw_input()) 
+  # input the array 
+arr = [int(x) for x in raw_input().split()] 
+  # initialize variable 
+summation = 0
+  # calculate sum 
+for x in arr: 
+    summation += x 
+    # print answer 
+print(summation) 
